db40b98bf58a94bdb55fd453cedbf7a05c159ddd
MaxTrgb <126002400+MaxTrgb@users.noreply.github.com>
2024-03-18 21:39:51 +0200

    begining

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// Watchers for scss and html files
+import "./index.scss";
+import "./scripts/watchers/_pages"
+
+// Import modules
+import "./scripts/utils/form"
+
+// Import methods
+
+// Bisiness logic
+
+// For pages
+const GENERAL_PATH = window.location.pathname
+console.log(GENERAL_PATH)
